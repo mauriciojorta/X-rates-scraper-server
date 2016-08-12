3e8f7b2c2f332e0091eeb51c729b30ae123ef4fa
Mauricio José Orta Rodríguez <sextus70@gmail.com>
2016-08-12 12:46:48 +0100

    Fix port

--- index.js
+++ index.js
@@ -32,7 +32,7 @@ app.get('/', function (req, res) {
  	
 });
 
-var server = app.listen(9000, function () {
+var server = app.listen(process.env.PORT, function () {
   var host = server.address().address;
   var port = server.address().port;
 
